49d6083ad2eaa004453914129c34080bc9b627a4
Asger <asgervb@gmail.com>
2024-02-27 10:47:48 +0100

    Initial commit

---
+++ src/main/java/com/example/ovelsegithubactions/OvelseGithubActionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ovelsegithubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvelseGithubActionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OvelseGithubActionsApplication.class, args);
+    }
+
+}
